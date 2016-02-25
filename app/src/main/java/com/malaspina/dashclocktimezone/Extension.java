a263d31008c334dd2af1a1ff9b0b42e55db7ded8
Ryan Malaspina <ryan.malaspina@gmail.com>
2016-02-24 23:09:06 -0500

    Initial Extension class

---
+++ app/src/main/java/com/malaspina/dashclocktimezone/Extension.java
@@ -0,0 +1,29 @@
+package com.malaspina.dashclocktimezone;
+
+import com.google.android.apps.dashclock.api.DashClockExtension;
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+/**
+ * Created by Ryan on 2/24/2016.
+ */
+public class Extension extends DashClockExtension {
+
+    @Override
+    protected void onInitialize(boolean isReconnect) {
+        setUpdateWhenScreenOn(true);
+        super.onInitialize(isReconnect);
+    }
+
+    @Override
+    protected void onUpdateData(int reason) {
+        ExtensionData data = new ExtensionData();
+
+        data.visible(true);
+        data.icon(R.mipmap.ic_stat);
+        data.status("Hi!");
+        data.expandedTitle("Hello World!");
+        data.expandedBody("Hello World! (Again!)");
+
+        publishUpdate(data);
+    }
+}
