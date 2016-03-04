7faa46f90b99fb14de8744184f614792231dc1fa
Ryan Malaspina <ryan.malaspina@gmail.com>
2016-03-04 00:53:30 -0500

    Three separate extensions

---
+++ app/src/main/java/com/malaspina/dashclocktimezone/Extension_1.java
@@ -0,0 +1,12 @@
+package com.malaspina.dashclocktimezone;
+
+/**
+ * Created by Ryan on 3/4/2016.
+ */
+public class Extension_1 extends ExtensionBase {
+    @Override
+    public void onCreate() {
+        // Set variables here
+        super.onCreate();
+    }
+}
