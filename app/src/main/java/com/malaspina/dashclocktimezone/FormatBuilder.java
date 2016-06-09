0c517b33d0cfe643bc6afeea3732ba7c7159fd7d
Ryan Malaspina <ryan.malaspina@gmail.com>
2016-06-08 21:03:26 -0400

    Assign views and adapters for TFB

--- app/src/main/java/com/malaspina/dashclocktimezone/FormatBuilder.java
+++ app/src/main/java/com/malaspina/dashclocktimezone/FormatBuilder.java
@@ -3,10 +3,15 @@ package com.malaspina.dashclocktimezone;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
@@ -17,13 +22,14 @@ import android.widget.TextView;
  * Created by Ryan on 6/8/2016.
  *
  */
-public class FormatBuilder extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
+public class FormatBuilder extends AppCompatActivity
+        implements CompoundButton.OnCheckedChangeListener, AdapterView.OnItemSelectedListener {
 
     private TextView preview, advancedNote;
     private Spinner timeSpinner, dateSpinner;
     private EditText customPattern;
     private CheckBox useAdvanced;
-    private Bundle intent;
+    private Bundle bundle;
 
     private String format;
     private SharedPreferences preferences;
@@ -32,10 +38,40 @@ public class FormatBuilder extends AppCompatActivity implements CompoundButton.O
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.format_builder);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setupActionBar();
+
+        bundle = getIntent().getExtras();
+        if (bundle == null) {
+            bundle = new Bundle();
+        }
+
+        preview = (TextView) findViewById(R.id.timeFormatBuilderLivePreviewTextView);
+        timeSpinner = (Spinner) findViewById(R.id.timeSpinner);
+        dateSpinner = (Spinner) findViewById(R.id.dateSpinner);
+        customPattern = (EditText) findViewById(R.id.timeFormatBuilderCustomPatternEditText);
+        useAdvanced = (CheckBox) findViewById(R.id.advancedCheckBox);
+        advancedNote = (TextView) findViewById(R.id.advancedNote);
+
+        useAdvanced.setOnCheckedChangeListener(this);
+
+        preferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_spinner_item,
+                getResources().getStringArray(R.array.time_format_builder_preset_entries));
+
+        timeSpinner.setAdapter(adapter);
+        dateSpinner.setAdapter(adapter);
+
+        customPattern.addTextChangedListener(customPatternWatcher);
     }
 
-
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
 
     TextWatcher customPatternWatcher = new TextWatcher() {
         @Override
@@ -54,6 +90,14 @@ public class FormatBuilder extends AppCompatActivity implements CompoundButton.O
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {}
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
