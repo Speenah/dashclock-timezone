6689ffad8f10e9e84d9f8202fe49a2c148f5bd99
Ryan Malaspina <ryan.malaspina@gmail.com>
2016-06-11 19:54:03 -0400

    Change extension names to match previous version

--- app/src/main/java/com/malaspina/dashclocktimezone/Extension2.java
+++ app/src/main/java/com/malaspina/dashclocktimezone/Extension2.java
@@ -3,7 +3,7 @@ package com.malaspina.dashclocktimezone;
 /**
  * Created by Ryan on 3/4/2016.
  */
-public class Extension_2 extends ExtensionBase {
+public class Extension2 extends ExtensionBase {
     @Override
     public void onCreate() {
         setTimezone_key(getString(R.string.prefs_select_timezone_key_2));
--- app/src/main/java/com/malaspina/dashclocktimezone/Extension3.java
+++ app/src/main/java/com/malaspina/dashclocktimezone/Extension3.java
@@ -3,7 +3,7 @@ package com.malaspina.dashclocktimezone;
 /**
  * Created by Ryan on 3/4/2016.
  */
-public class Extension_3 extends ExtensionBase {
+public class Extension3 extends ExtensionBase {
     @Override
     public void onCreate() {
         setTimezone_key(getString(R.string.prefs_select_timezone_key_3));
